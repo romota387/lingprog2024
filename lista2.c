60fca18c838289ad7d659acf88c5d9a2f0a28ded
romota387 <romota387@gmail.com>
2024-12-14 00:18:08 +0000

    lista2 alterado 10

--- lista2.c
+++ lista2.c
@@ -200,30 +200,30 @@ void questao10() {
 	printf("Digite o numero 3: ");
 	scanf("%d", &num3);
 	
-	if(	num1 < num2 && num1 < num3  ){
-		if (num2 < num3)
+	if(	num1 < num2 && num2 < num3  ){
 		printf("%d, %d, %d", num1, num2,num3);
+	} 
+	else if (num1 < num3 && num3< num2) {
+		printf (" %d, %d, %d", num1, num3, num2 );
+	} 
+	else if (num2 < num1 && num1 < num3){
+		printf("%d, %d, %d", num2, num1, num3);
+	} 
+	else if ( num2 < num3 && num3 < num1 ) {
+		printf("%d, %d, %d", num2, num3, num1);
+	} 
+	else if (num3 < num1 && num1 < num2){
+		printf("%d, %d, %d", num3, num2, num1);
 	}
-	else
-	printf (" %d, %d, %d", num1, num3, num2 );
-	 }
-	else if (num2 < num1 && num2 < num3){
-	if (num1 < num3)
-	printf("%d, %d, %d", num2, num1, num3);
-	else
-	printf("%d, %d, %d", num2, num3, num1)
+	else if (num3 < num2 && num2 < num1){
+		printf("%d, %d, %d", num3, num1, num2);
 	}
 	else{
-		if( num2 < num1)
-		printf("%d, %d, %d", num3, num2, num1);
-		else
-		printf("%d, %d, %d", num3, num1, num2);
-	
-	
-	
+		printf("%d, %d, %d", num1, num2, num3);
+	}
 }
+	
 
-}
 
 //11. Faça um programa que leia 3 números e imprima o maior deles.
 void questao11() {
